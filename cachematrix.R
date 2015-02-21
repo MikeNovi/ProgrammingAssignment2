4a36cf24c30c0aece96ba367d8a2ad31ea98961e
Michael Novi <me@michaelnovi.com>
2015-02-20 19:44:38 -0500

    Fixed some spelling errors in existing comments and commented the cacheSolve function to better explain the sections of code within the function

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,5 @@
 ## Datastructure and function for performing, caching, and retrieving
-## 
+## the inverse of a a matrix.
 
 ## Creates a datastructure that contains a matrix, cached inverse,
 ## and accessors for getting/setting the inverse value.
@@ -21,15 +21,22 @@ makeCacheMatrix <- function(x = matrix()) {
 
 ## Function using the makeCacheMatrix to 'solve' for the inverse
 ## of a supplied matrix. Calculates the inverse on first attempt 
-## and relies upon the cached inverse value on ubsequent attempts.
+## and relies upon the cached inverse value on subsequent attempts.
 
 cacheSolve <- function(x, ...) {
   ## Return a matrix that is the inverse of 'x'
   m <- x$getinv()
+  
+  # If there is an cached inverse indicate that a cached value is 
+  # being returned and return it
   if(!is.null(m)) {
     message("getting cached data")
     return(m)
   }
+  
+  # If there is no cached value, get the matrix, solve for its
+  # inverse, store the inverse in the data structure, then
+  # return the fresh inverse.
   mtx <- x$get()
   m <- solve(mtx, ...)
   x$setinv(m)
